c37184cb980a3b12d890e5ddeed2e4e6bb9abc25
thototh <144813512+thototh@users.noreply.github.com>
2026-03-13 12:30:32 +0900

    Update 3-2.c

--- code/3-2.c
+++ code/3-2.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
-int main()
-{
+int main(void){
+
+    int a = -1;
+    unsigned int b = 1;
+     if (a<b)
+     {
+        printf("a < b\n"); 
+     }else{
+        printf("a >= b ???\n");
+     }
+     
 
     return 0;
 }
