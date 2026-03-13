40d4ab951ccb78d060256728d4daa78273cf18ec
thototh <144813512+thototh@users.noreply.github.com>
2026-03-13 17:54:17 +0900

    Update 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-int main()
-{
+int main(void){
+    int num;
+    bool isPrime = true;
+
+    scanf("%d", &num);
+
+    if(num <= 1){
+        isPrime = false;
+    }
+    else if(num == 2){
+        isPrime = true;
+    }
+    else if(num % 2 == 0){
+        isPrime = false;
+    }
+    else{
+        for (int i = 3; i*i <= num; i += 2){
+            if (num % i == 0){
+                isPrime = false;
+                break;
+            }
+        }
+    }
+
+    printf("%s\n", isPrime ? "true" : "false");
 
     return 0;
 }
-
