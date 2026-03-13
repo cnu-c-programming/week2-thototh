420ec10af3a7764d64d5cd58907b99cc155dbb87
thototh <144813512+thototh@users.noreply.github.com>
2026-03-13 11:58:33 +0900

    Update 1-2.c

--- code/1-2.c
+++ code/1-2.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
-int main()
-{
 
+int main(void){
 
+    printf("% 10s ", "name");
+    printf("% 10s ", "id");
+    printf("% 10s\n", "value");
+
+    printf("% 10s ", "kim");
+    printf("%010d ", 12);
+    printf("% 10.2f\n", 4.120000);
+
+    printf("% 10s ", "lee");
+    printf("%010d ", 1922);
+    printf("% 10.2f\n", 3.210000);
+
+    printf("% 10s ", "park");
+    printf("% 010d ", 432);
+    printf("% 10.2f", 1.000000);
     return 0;
 }
