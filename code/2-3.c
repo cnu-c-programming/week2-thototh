e528632474b144cc2240e4a5061a7bafa6a13455
thototh <144813512+thototh@users.noreply.github.com>
2026-03-13 12:19:25 +0900

    Update 2-3.c

--- code/2-3.c
+++ code/2-3.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
-int main()
-{
+int main(void){
 
+    unsigned int x = 0x12345678;
+
+    unsigned char* addr = (unsigned char*)&x;
+
+    printf("%x\n", *addr);
+    printf("%x\n", *(addr + 1));
+    printf("%x\n", *(addr + 2));
+    printf("%x\n", *(addr + 3));
     return 0;
 }
-
