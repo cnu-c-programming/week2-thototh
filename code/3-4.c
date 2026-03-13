59e4a3141c25772ca734ddd39ce8c16782b4ed72
thototh <144813512+thototh@users.noreply.github.com>
2026-03-13 12:34:22 +0900

    Update 3-4.c

--- code/3-4.c
+++ code/3-4.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 
-int main()
-{
+int main(void){
+
+    int a,b,c;
+    
+    a = b = c = 5;
+    printf("%d %d %d\n", a, b, c);
+
+    a = 2 + 3 * 4;
+    printf("%d %d %d\n", a, b, c);
+
+    c = a++ + ++b;
+    printf("%d %d %d\n", a, b, c);
+     
 
     return 0;
 }
-
